e34dc79ff8dbd14ac05f4d10e27a206301987048
Christer-L <christer.lohk@gmail.com>
2024-02-15 15:55:06 +0100

    Fix bugs in segmentation script

--- 2_segment.py
+++ 2_segment.py
@@ -36,7 +36,7 @@ def segment(path_list, gpu_id, kernel_size, data_dir, out_dir):
     # creates a pretrained model
     model = StarDist2D.from_pretrained('2D_versatile_fluo')
 
-    save_dir = os.path.join(out_dir, "gaussian_{}".format(kernel_size))
+    save_dir = os.path.join(out_dir, "gaussian_{}".format(int(kernel_size)))
     os.makedirs(save_dir, exist_ok=True)
 
     for path in paths:
@@ -46,14 +46,16 @@ def segment(path_list, gpu_id, kernel_size, data_dir, out_dir):
 
         # In OpenCV, if you set sigma to 0, it calculates an appropriate sigma based on the kernel size,
         # making the process somewhat adaptive.
+        kernel_size = int(kernel_size)
         blurred_image = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
         labels, _ = model.predict_instances(normalize(blurred_image))
 
         # Save segmentation
         labels_16bit = map_to_16bit(labels).astype(np.uint16)
 
-        os.makedirs(directory, exist_ok=True)
-        save_path = os.path.join(save_dir, path)
+        image_save_dir = os.path.join(save_dir, directory)
+        os.makedirs(image_save_dir, exist_ok=True)
+        save_path = os.path.join(image_save_dir, os.path.basename(path))
         tifffile.imwrite(save_path, labels_16bit)
 
 
@@ -61,20 +63,22 @@ def main():
     parser = argparse.ArgumentParser(description="Segmentation")
 
     # Add arguments
-    parser.add_argument("path_list_file", type=str, help="Path to the pickle file with all DAPI paths")
-    parser.add_argument("gpu_id", type=str, help="GPU ID")
-    parser.add_argument("kernel_size", type=int, help="Gaussian Kernel Size")
-    parser.add_argument("data_path", type=str, help="Input data path")
-    parser.add_argument("out_dir", type=str, help="Segmentation directory")
+    parser.add_argument("--path_list_file", type=str, help="Path to the pickle file with all DAPI paths",
+            nargs='?', default="/home/clohk/datasets/mcf7/metadata/DAPI_paths.pkl")
+    parser.add_argument("--gpu_id", type=str, help="GPU ID")
+    parser.add_argument("--kernel_size", type=str, help="Gaussian Kernel Size")
+    parser.add_argument("--data_path", type=str, help="Input data path",
+            nargs='?', default="/home/clohk/datasets/mcf7/raw_extracted")
+    parser.add_argument("--out_dir", type=str, help="Segmentation directory", 
+            nargs='?', default="/mnt/cbib/image_datasets/christer_datasets/mcf7/segmentations")
 
     # Parse arguments
     args = parser.parse_args()
-    segment(args.config_file, args.gpu_id, args.kernel_size, args.data_path, args.out_dir)
+
+    segment(args.path_list_file, args.gpu_id, args.kernel_size, args.data_path, args.out_dir)
 
 
 if __name__ == '__main__':
-    segment("home/christer/Datasets/MCF7/structured/metadata/DAPI_paths.pkl",
-            "0", 3,
-            "/home/christer/Datasets/MCF7/raw_extracted",
-            "/home/christer/Datasets/MCF7/structured/segmentations")
+    main()    
+
 
