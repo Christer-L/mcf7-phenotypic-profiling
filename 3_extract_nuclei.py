3d555a5d78e9f792b4c5e1c76817a317bfc5e93c
Christer-L <christer.lohk@gmail.com>
2024-03-08 17:50:54 +0100

    Update nucleus extraction

--- 3_extract_nuclei.py
+++ 3_extract_nuclei.py
@@ -1,22 +1,84 @@
 import os
 from glob import glob
-import cv2
 import tifffile
-import numpy as np
-from scipy import ndimage
-import torch
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
 import argparse
+import numpy as np
 import time
-import math
-import torch
-import torchvision.transforms.functional as TF
+import cv2
+from tqdm import tqdm
 
 import torch
+import torch.nn.functional as F
+import torchvision.transforms.functional as TF
+
 import numpy as np
 import SimpleITK as sitk
 from skimage import draw
+import matplotlib.pyplot as plt
+
+
+def register_image_to_circle(img_np, mask_np, image_size=128, circle_radius=50):
+
+    plt.imshow(img_np)
+    plt.show()
+
+    # Ensure image and mask are single channel and convert mask to binary format if it's not
+    mask_np = (mask_np > 0).astype(np.uint8)
+
+    # Create Circle Mask for Target Registration
+    black = np.zeros((image_size, image_size))
+
+    # Define center and radius for the disk
+    center = (image_size // 2, image_size // 2)  # Center of the disk, using integer division for indices
+    radius = circle_radius  # Radius of the disk
+
+    # Generate disk coordinates
+    rr, cc = draw.disk(center, radius, shape=(image_size, image_size))
+
+    # Apply the disk to the image
+    black[rr, cc] = 1  # This directly uses rr and cc for indexing
+
+    circle_mask = black.astype('uint8')
+
+    plt.imshow(circle_mask)
+    plt.show()
+
+    # Initialize Registration Method
+    selx = sitk.ElastixImageFilter()
+    selx.SetParameterMap(selx.GetDefaultParameterMap('nonrigid'))
+    selx.LogToConsoleOff()
+
+    # Convert numpy arrays back to SimpleITK Images
+    movingImage = sitk.GetImageFromArray(mask_np)
+    movingLabel = sitk.GetImageFromArray(img_np)
+    fixedImage = sitk.GetImageFromArray(circle_mask)
+
+    # Create Deformation Field necessary to register shape of mask to target circle
+    selx.SetMovingImage(movingImage)
+    selx.SetFixedImage(fixedImage)
+    selx.Execute()
+
+    # Transform Image using the deformation field from above
+    resultImage = sitk.Transformix(movingLabel, selx.GetTransformParameterMap())
+
+    # Convert result back to np.array
+    registered_image_np = sitk.GetArrayFromImage(resultImage)
+    registered_image_np = np.reshape(registered_image_np, (image_size, image_size))
+    registered_image_np[registered_image_np < 0] = 0
+
+    return registered_image_np.astype('uint8')
+
+
+def smooth_mask(mask):
+    contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    smoothed_contours = [cv2.approxPolyDP(cnt, 0.05, True) for cnt in contours]
+    mask_smoothed = np.zeros_like(mask)
+    cv2.drawContours(mask_smoothed, smoothed_contours, -1, (1), thickness=cv2.FILLED)
+    return mask_smoothed
+
+
+def normalize_single_nuc(img):
+    return (img - img.min()) / (img.max() - img.min()) * 255
 
 
 def compute_orientation(mask):
@@ -110,19 +172,17 @@ def check_mass_center(mask):
     return is_centered
 
 
-def extract_cells(img_path, segmentation_path, img_out_dir, seg_out_dir, dim=128,
-                  normalize_image=True, normalize_cell_image=True):
+def extract_cells(img_path, segmentation_path, img_out_dir, seg_out_dir, device,
+                  dim=128,
+                  normalize_image=True):
     if dim % 2 != 0:
         print("(!) Output image dimension must be even")
         raise ValueError()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("---->>>> DEVICE: {}".format(device))
-
     img_np = tifffile.imread(img_path)
 
     if normalize_image:
-        img_np = ((img_np - img_np.min()) / (img_np.max() - img_np.min()) * 255).astype(np.uint8)
+        img_np = ((img_np - img_np.min()) / (img_np.max() - img_np.min()) * 255)
 
     mask_np = tifffile.imread(segmentation_path)
 
@@ -131,7 +191,7 @@ def extract_cells(img_path, segmentation_path, img_out_dir, seg_out_dir, dim=128
     padding = (padding_val,) * 4
 
     # Load images and masks into PyTorch tensors
-    img = torch.tensor(img_np.astype(np.int8), device=device)
+    img = torch.tensor(img_np.astype(np.uint8), device=device)
     mask = torch.tensor(mask_np.astype(np.int16), device=device)
 
     padded_img = F.pad(img, padding, "constant", 0)
@@ -152,42 +212,40 @@ def extract_cells(img_path, segmentation_path, img_out_dir, seg_out_dir, dim=128
             continue
 
         # Create a mask for the current nucleus
-        nucleus_mask = (padded_mask == label).float()
+        nucleus_mask = (padded_mask == label)
 
         roi_mask, roi_img = center_crop_around_com(padded_img, nucleus_mask, dim)
 
-        # Filter image with mask and move tensors back to CPU for saving
+        nucleus_filtered = roi_img * roi_mask
 
-        mask_filtered = roi_mask.cpu().numpy()
+        nucleus_filtered_rotated, mask_filtered_rotated = rotate_centered_cell(nucleus_filtered, roi_mask)
 
-        # contours, _ = cv2.findContours(mask_filtered.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # smoothed_contours = [cv2.approxPolyDP(cnt, 0.05, True) for cnt in contours]  # Adjust epsilon for smoothing
-        # mask_smoothed = np.zeros_like(mask_filtered)
-        # cv2.drawContours(mask_smoothed, smoothed_contours, -1, (1), thickness=cv2.FILLED)
+        norm_nucleus_filtered = normalize_single_nuc(nucleus_filtered)
+        norm_nucleus_filtered_rotated = normalize_single_nuc(nucleus_filtered_rotated)
 
-        nucleus_filtered = (roi_img.cpu() * mask_filtered.astype(np.uint8)).cpu().numpy()
-
-        nucleus_filtered_rotated, mask_filtered_rotated = rotate_centered_cell(torch.Tensor(nucleus_filtered),
-                                                                               torch.Tensor(mask_filtered))
-
-        if normalize_cell_image:
-            nucleus_filtered = ((nucleus_filtered - nucleus_filtered.min()
-                                 ) / (nucleus_filtered.max() - nucleus_filtered.min()) * 255).astype(np.uint8)
-            nucleus_filtered_rotated = ((nucleus_filtered_rotated - nucleus_filtered_rotated.min()
-                                         ) / (nucleus_filtered_rotated.max() - nucleus_filtered_rotated.min()
-                                              ) * 255).cpu().numpy().astype(np.uint8)
+        mask_to_save = roi_mask.cpu().numpy().astype(np.uint8)
+        nuc_to_save = nucleus_filtered.cpu().numpy().astype(np.uint8)
+        rot_nuc_to_save = nucleus_filtered_rotated.cpu().numpy().astype(np.uint8)
+        rot_mask_to_save = mask_filtered_rotated.cpu().numpy().astype(np.uint8)
+        norm_rot_nuc_to_save = norm_nucleus_filtered_rotated.cpu().numpy().astype(np.uint8)
+        norm_nuc_to_save = norm_nucleus_filtered.cpu().numpy().astype(np.uint8)
 
         # Save the centered nucleus and its mask
         name = f"{label.item()}.tif"
         tifffile.imwrite(os.path.join(img_out_dir, "original_{}".format(name)),
-                         nucleus_filtered.astype(np.uint8))
+                         nuc_to_save)
         tifffile.imwrite(os.path.join(seg_out_dir, "original_{}".format(name)),
-                         mask_filtered.astype(np.uint8))
+                         mask_to_save)
 
         tifffile.imwrite(os.path.join(img_out_dir, "rotated_{}".format(name)),
-                         nucleus_filtered_rotated.astype(np.uint8))
+                         rot_nuc_to_save)
         tifffile.imwrite(os.path.join(seg_out_dir, "rotated_{}".format(name)),
-                         mask_filtered_rotated.cpu().numpy().astype(np.uint8))
+                         rot_mask_to_save)
+
+        tifffile.imwrite(os.path.join(img_out_dir, "rotated_normalized_{}".format(name)),
+                         norm_rot_nuc_to_save)
+        tifffile.imwrite(os.path.join(img_out_dir, "original_normalized_{}".format(name)),
+                         norm_nuc_to_save)
 
 
 def main():
@@ -195,17 +253,20 @@ def main():
 
     # Add arguments
     parser.add_argument("--image_dir", type=str, help="Image directory path (dir that contains plate dirs)",
-                        nargs='?', default="/home/clohk/datasets/mcf7/raw_extracted")
-    parser.add_argument("--gpu_id", type=str, help="GPU ID")
+                        nargs='?', default="/home/christer/Datasets/MCF7/raw_extracted")
+    parser.add_argument("--gpu_id", type=str, help="GPU ID", nargs='?', default="0")
     parser.add_argument("--segmentations_dir", type=str, help="Segmentations directory path",
-                        nargs='?', default="/mnt/cbib/image_datasets/christer_datasets/mcf7/segmentations/gaussian_3")
+                        nargs='?', default="/home/christer/Datasets/MCF7/structured/segmentations/gaussian_3")
     parser.add_argument("--out_dir", type=str, help="output directory",
-                        nargs='?', default="/mnt/cbib/image_datasets/christer_datasets/mcf7/extracted_cells")
+                        nargs='?', default="/home/christer/Datasets/MCF7/structured/extracted_cells")
 
     # Parse arguments
     args = parser.parse_args()
 
-    for seg_path in glob(os.path.join(args.segmentations_dir, "*", "*")):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("---->>>> DEVICE: {}".format(device))
+
+    for seg_path in tqdm(glob(os.path.join(args.segmentations_dir, "*", "*"))):
         path_parts = os.path.normpath(seg_path).split(os.sep)
         gaussian_dir = path_parts[-3]
         plate_dir = path_parts[-2]
@@ -218,7 +279,7 @@ def main():
 
         img_path = os.path.join(args.image_dir, plate_dir, file_name)
 
-        extract_cells(img_path, seg_path, img_save_dir, seg_save_dir)
+        extract_cells(img_path, seg_path, img_save_dir, seg_save_dir, device)
 
 
 if __name__ == '__main__':
