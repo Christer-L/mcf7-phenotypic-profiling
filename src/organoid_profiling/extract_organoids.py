0a786110f6992eb3e4b54590844c710f7f357dde
Christer-L <christer.lohk@gmail.com>
2024-05-13 14:52:41 +0200

    Change number of workers

--- src/organoid_profiling/extract_organoids.py
+++ src/organoid_profiling/extract_organoids.py
@@ -165,7 +165,7 @@ def main():
 
         task_args.append((img_path, seg_path, img_save_dir, seg_save_dir, int(slice_idx)))
 
-    with ProcessPoolExecutor(max_workers=1) as executor:
+    with ProcessPoolExecutor() as executor:
         futures = [executor.submit(extract_objects, arg) for arg in task_args]
         for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
             pass
--- src/organoid_profiling/scripts/extract_organoids.sh
+++ src/organoid_profiling/scripts/extract_organoids.sh
@@ -4,8 +4,8 @@
 #SBATCH -e slurm/slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
 #SBATCH --job-name=object_extraction
 #SBATCH --time=10:00:00
-#SBATCH --ntasks=30
-#SBATCH --mem=196G
+#SBATCH --ntasks=4
+#SBATCH --mem=80G
 
 cd ..
 
