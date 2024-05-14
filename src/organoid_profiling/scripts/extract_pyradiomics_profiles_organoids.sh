ce27919c3c5960387c8f6000e413148f0fb80b7b
Christer-L <christer.lohk@gmail.com>
2024-05-14 11:00:38 +0200

    Modified slurm script.

--- src/organoid_profiling/scripts/extract_pyradiomics_profiles_organoids.sh
+++ src/organoid_profiling/scripts/extract_pyradiomics_profiles_organoids.sh
@@ -4,9 +4,13 @@
 #SBATCH -e slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
 #SBATCH --job-name=feature_extraction
 #SBATCH --time=10:00:00
-#SBATCH --ntasks=4
-#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
 
-cd ../../..
+cd ..
+cd ..
+cd ..
+
+
 
 conda run -n pyradiomics python -m src.organoid_profiling.extract_pyradiomics_profiles_organoids
