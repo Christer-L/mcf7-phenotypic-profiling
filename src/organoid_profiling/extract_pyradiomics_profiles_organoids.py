46c2c67d903dc71161920476aa6d93eeab262817
Christer-L <christer.lohk@gmail.com>
2024-05-14 11:37:28 +0200

    Set limit to number of workers

--- src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
+++ src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
@@ -81,7 +81,7 @@ def main():
     for dir_path in dirs:
         task_args.append((dir_path, extractor, args.out_dir, args.extracted_objects_dir))
 
-    with ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers=1) as executor:
         futures = [executor.submit(extract_features, arg) for arg in task_args]
         for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
             pass
