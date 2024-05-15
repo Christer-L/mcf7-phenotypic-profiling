e09f221b7140fe5c015bcec9e15105666cf6e6f4
Christer-L <christer.lohk@gmail.com>
2024-05-15 17:38:19 +0200

    Add config for pyradiomics

--- src/pipeline/extract_pyradiomics_profiles.py
+++ src/pipeline/extract_pyradiomics_profiles.py
@@ -84,7 +84,7 @@ def main():
     # Parse arguments
     args = parser.parse_args()
 
-    extractor = featureextractor.RadiomicsFeatureExtractor()
+    extractor = featureextractor.RadiomicsFeatureExtractor("pipeline/config/pyradiomics2D.yaml")
 
     # Get all paths
     paths = []
