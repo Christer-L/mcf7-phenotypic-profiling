8013ebb4611c7611163129bd04f5704b39c9277a
Christer-L <christer.lohk@gmail.com>
2024-05-15 19:16:22 +0200

    Add config for pyradiomics

--- src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
+++ src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
@@ -79,9 +79,9 @@ def main():
     dirs = ["/".join(path.split("/")[-2:]) for path in paths]
 
     task_args = []
-    # for dir_path in dirs:
-    #     task_args.append((dir_path, extractor, args.out_dir, args.extracted_objects_dir))
-    task_args.append(("filtered_ctrl/52", extractor, args.out_dir, args.extracted_objects_dir))
+    for dir_path in dirs:
+        task_args.append((dir_path, extractor, args.out_dir, args.extracted_objects_dir))
+    #task_args.append(("filtered_ctrl/52", extractor, args.out_dir, args.extracted_objects_dir))
 
     with ProcessPoolExecutor(max_workers=1) as executor:
         futures = [executor.submit(extract_features, arg) for arg in task_args]
