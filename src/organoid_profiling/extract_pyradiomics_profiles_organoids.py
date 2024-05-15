dca529cca3128e90e9c873ad32917ea6a68c138e
Christer-L <christer.lohk@gmail.com>
2024-05-15 19:13:50 +0200

    Add config for pyradiomics

--- src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
+++ src/organoid_profiling/extract_pyradiomics_profiles_organoids.py
@@ -72,7 +72,7 @@ def main():
     # Parse arguments
     args = parser.parse_args()
 
-    extractor = featureextractor.RadiomicsFeatureExtractor("pipeline/config/pyradiomics2D.yaml")
+    extractor = featureextractor.RadiomicsFeatureExtractor("src/pipeline/config/pyradiomics2D.yaml")
 
     # Get all paths
     paths = glob(os.path.join(args.extracted_objects_dir, "images", "*", "*"))
