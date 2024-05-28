aa9f03f75a042fb6118d7f1711651eddd3b6b255
Christer-L <christer.lohk@gmail.com>
2024-05-28 15:02:32 +0200

    Modify slurm script.

--- scripts/extract_image_objects.sh
+++ scripts/extract_image_objects.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -o slurm/slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
-#SBATCH -e slurm/slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
+#SBATCH -o slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
+#SBATCH -e slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
 #SBATCH --job-name=object_extraction
 #SBATCH --time=10:00:00
 #SBATCH --ntasks=30
 #SBATCH --mem=196G
 
-conda run -n cells2circles python ../src/extract_nuclei.py
+conda run -n cells2circles python ../src/pipeline/extract_image_objects.py
