450ec77f3fe1f5d6c40beefabd96f45ede733ab1
Christer-L <christer.lohk@gmail.com>
2024-06-03 11:48:37 +0200

    Modify slurm script

--- scripts/extract_pyradiomics_profiles.sh
+++ scripts/extract_pyradiomics_profiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH -o slurm/slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
-#SBATCH -e slurm/slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
+#SBATCH -o slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
+#SBATCH -e slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
 #SBATCH --job-name=feature_extraction
 #SBATCH --time=10:00:00
 #SBATCH --ntasks=12
