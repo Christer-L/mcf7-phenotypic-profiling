8d4b04fef6742561b7e2bedb78f6bd29572ffece
Christer-L <christer.lohk@gmail.com>
2024-07-12 15:52:17 +0200

    Change slurm parameters

--- src/experiments/hepaRG_canaliculi_3D/profile_slurm.py
+++ src/experiments/hepaRG_canaliculi_3D/profile_slurm.py
@@ -106,7 +106,7 @@ def main():
         mask = tifffile.imread(path)
         task_args.append((path, mask, extractor, condition, concentration, args.min_volume, args.out_path))
 
-    with ProcessPoolExecutor(max_workers=16) as executor:
+    with ProcessPoolExecutor(max_workers=8) as executor:
         futures = [executor.submit(extract_features, arg) for arg in task_args]
         for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
             pass
