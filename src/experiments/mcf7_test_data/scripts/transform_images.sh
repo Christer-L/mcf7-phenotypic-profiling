93bebf51ad9879bf616d74afa019389251abc01d
Christer-L <christer.lohk@gmail.com>
2024-09-02 14:27:05 +0200

    Add functionality for transforming nucleus shape for the dummy dataset

--- src/experiments/mcf7_test_data/scripts/transform_images.sh
+++ src/experiments/mcf7_test_data/scripts/transform_images.sh
@@ -2,16 +2,14 @@
 
 #SBATCH -o slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
 #SBATCH -e slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
-#SBATCH --job-name=cell_extraction
+#SBATCH --job-name=transform_images
 #SBATCH --time=10:00:00
-#SBATCH --ntasks=8
-#SBATCH --mem=90G
+#SBATCH --ntasks=32
+#SBATCH --mem=190G
 
-cd ..
-cd ..
-cd ..
-cd ..
+cd ../../../..
 
 pwd
 
-conda run -n pyradiomics python -m src.experiments.mcf7_test_data.1_generate_images
+conda run -n cells2circles python -m src.experiments.mcf7_test_data.2_transform_images
+
