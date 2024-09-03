7d3f47cae8196a1736a88347af25f2fa215c5fed
Christer-L <christer.lohk@gmail.com>
2024-09-03 12:57:31 +0200

    Increase time limit for slurm

--- src/experiments/mcf7_test_data/scripts/transform_images.sh
+++ src/experiments/mcf7_test_data/scripts/transform_images.sh
@@ -3,7 +3,7 @@
 #SBATCH -o slurm.%x.%N.%j.out  # STDOUT file with the Job name, the Node name and the Job ID
 #SBATCH -e slurm.%x.%N.%j.err  # STDERR file with the Job name, the Node name and the Job ID
 #SBATCH --job-name=transform_images
-#SBATCH --time=10:00:00
+#SBATCH --time=72:00:00
 #SBATCH --ntasks=32
 #SBATCH --mem=190G
 
